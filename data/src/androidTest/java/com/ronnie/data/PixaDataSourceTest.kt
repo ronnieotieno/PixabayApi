3846367e86f62c64be7a67fa5e1d495f3c893ed9
Ronnie Otieno <ronnieotieno2@gmail.com>
2021-12-13 10:04:17 +0300

    Add datasource test
    Signed-off-by: Ronnie Otieno <ronnieotieno2@gmail.com>

---
+++ data/src/androidTest/java/com/ronnie/data/PixaDataSourceTest.kt
@@ -0,0 +1,91 @@
+package com.ronnie.data
+import android.content.Context
+import androidx.paging.PagingSource
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.ronnie.data.api.PixaBayApi
+import com.ronnie.data.datasource.PixaDataSource
+import com.ronnie.domain.models.Image
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.hamcrest.CoreMatchers
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.util.concurrent.TimeUnit
+
+
+class PixaDataSourceTest {
+    private var context: Context? = null
+    private var mockWebServer = MockWebServer()
+    private lateinit var apiService: PixaBayApi
+    private lateinit var pagingSource: PixaDataSource
+    private lateinit var imageList: List<Image>
+    private val gson = Gson()
+
+    @Before
+    fun setup() {
+        mockWebServer.start()
+
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        context = InstrumentationRegistry.getInstrumentation().targetContext
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+
+        apiService = Retrofit.Builder()
+            .baseUrl(mockWebServer.url("/"))
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(PixaBayApi::class.java)
+
+        val jsonStream: InputStream = context!!.resources.assets.open("response.json")
+        val jsonBytes: ByteArray = jsonStream.readBytes()
+
+
+        imageList = gson.fromJson(String(jsonBytes), object : TypeToken<List<Image>>() {}.type)
+
+        val response = MockResponse()
+            .setResponseCode(HttpURLConnection.HTTP_OK)
+            .setBody(String(jsonBytes))
+        mockWebServer.enqueue(response)
+        pagingSource = PixaDataSource("",apiService,context!!)
+
+    }
+
+    @After
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
+
+    @Test
+    fun loadReturnsPageWhenOnSuccessfulLoadOfItemKeyedData() = runBlocking {
+        assertThat(  PagingSource.LoadResult.Page(
+            data =  imageList,
+            prevKey = null,
+            nextKey = 2
+        ), CoreMatchers.equalTo( pagingSource.load(
+            PagingSource.LoadParams.Refresh(
+                key = null,
+                loadSize = 20,
+                placeholdersEnabled = false
+            )
+        )))
+    }
+
+}
